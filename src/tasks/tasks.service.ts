1e541fcb07d55c56590a18c58bcf1a93da59d3bd
Marco Arruda <marco.nc.arruda@gmail.com>
2022-04-27 15:30:33 -0300

    Create task entity and repository

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-tasks.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TasksRepository } from './tasks.repository';
 
 @Injectable()
 export class TasksService {
