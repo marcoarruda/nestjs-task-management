ea2445896d68663073695d9871682f2cbec3601a
Marco Arruda <marco.nc.arruda@gmail.com>
2022-05-07 19:28:51 -0300

    Adjust delete for user auth

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -19,9 +19,7 @@ export class TasksService {
   }
 
   async getTaskById(id: string, user: User): Promise<Task> {
-    const found = await this.tasksRepository.findOne({
-      where: { id, user },
-    });
+    const found = await this.tasksRepository.findOne({ id, user });
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
@@ -32,8 +30,8 @@ export class TasksService {
     return await this.tasksRepository.createTask(createTaskDto, user);
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete(id);
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
     if (result.affected === 0) {
       throw new NotFoundException(`Task with id "${id}" not found`);
     }
