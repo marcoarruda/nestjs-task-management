a3e5d18b581ee7a349f69aab0c3fe5868c0569e8
Marco Arruda <marco.nc.arruda@gmail.com>
2022-05-07 20:33:45 -0300

    Using configService to load typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,15 +10,19 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: [`.env.state.${process.env.STAGE}`],
     }),
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      port: 3306,
-      host: 'localhost',
-      database: 'nestjs_task_management',
-      username: 'nestjs_task_management',
-      password: 'Abc.1234',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        port: configService.get('DB_PORT'),
+        host: configService.get('DB_HOST'),
+        database: configService.get('DB_NAME'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     AuthModule,
   ],
