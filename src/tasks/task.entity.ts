b0cf1fff1fa4abe6fcd04ff8128bcb0537bd2afb
Marco Arruda <marco.nc.arruda@gmail.com>
2022-05-07 18:18:54 -0300

    Create entities relation

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 
 @Entity()
@@ -14,4 +15,8 @@ export class Task {
 
   @Column()
   status: TaskStatus;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
